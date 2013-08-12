8ce3aeea28e18a961ded4896001298a033ba31cd
Thomas Chemineau <thomas.chemineau@gmail.com>
2013-08-12 10:53:49 +0200

    add wrapper classes, init file and vendor library

---
+++ classes/Kohana/OS.php
@@ -0,0 +1,4 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Kohana_OS extends \Browser\OS {}
+
---
+++ init.php
@@ -0,0 +1,8 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+// Load the autoloader file
+require_once dirname(__FILE__).'/vendor/php-browser/src/Browser/Autoloader.php';
+
+// Register the module's autoloader
+\Browser\Autoloader::register();
+
